e4043011f3b96f2b20b4e7ef1c561903a8ea9d6b
0574MisterXu <578795607@qq.com>
2018-08-15 20:16:03 +0800

    Initial commit

---
+++ spring-cloud-starter-projects/eureka-server/src/main/java/com/misterxu/eureka/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.misterxu.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by misterxu on 2018/8/15.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
